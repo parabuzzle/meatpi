2ec85a3d12275ce7787da6ddba220343ef09b720
Mike Heijmans <parabuzzle@gmail.com>
2014-10-08 22:46:22 -0700

    initial setup

---
+++ lib/meatpi.rb
@@ -0,0 +1,10 @@
+
+# Load the lib path into the load_path
+$:.unshift(File.dirname(__FILE__))
+
+require 'meatpi/exceptions'
+require 'meatpi/version'
+
+module MeatPi
+
+end
---
+++ lib/meatpi/version.rb
@@ -0,0 +1,3 @@
+module MeatPi
+  VERSION = "0.0.1"
+end
